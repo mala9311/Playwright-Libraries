f4d0647b26308e4a310f27bb202088bf8798ae72
mala9311 <malakumari9311@gmail.com>
2026-04-07 13:30:01 +0530

    initial commit

--- second_text.py
+++ second_text.py
@@ -2,14 +2,16 @@ import asyncio
 from playwright.async_api import async_playwright
 
 async def main():
-    async with async_playwright as p:
-        browser = await p.chromium.launch(headless=False, slow_mo=2000)
-        page = await page.new_page()
-        page.goto('https://unsplash.com')
+    async with async_playwright() as p:
+        browser = await p.chromium.launch(headless=False, slow_mo=10000)
+        page = await browser.new_page()
+        await page.goto('https://unsplash.com')
         image_text=page.get_by_alt_text("a group of people sitting around a table with food")
-        image_text.highlight()
-        print(page.title())
-        print(browser.close())
+        await image_text.highlight()
+        print(await page.title())
+        await browser.close()
+
+asyncio.run(main())  
 
 
 
---
+++ test_saucedemo.py
@@ -0,0 +1,19 @@
+from playwright.sync_api import Page
+import pytest 
+# To skip one browser 
+# @pytest.mark.skip_browser("chromium")
+@pytest.mark.only_browser("chromium")
+def test_title(page:Page):
+    page.goto("/")
+    assert page.title() == "Swag Labs"
+
+def test_inventory(page:Page):
+    page.goto("/inventory.html")
+    assert page.inner_text('h3') == "Epic sadface: You can only access '/inventory.html' when you are logged in."
+# ### write in terminal commands
+# To run in both the browser -> pytest --headed --base-url https://www.saucedemo.com --browser chromium --browser firefox
+# def test_inventory(page: Page):
+#     page.goto("https://www.saucedemo.com/inventory.html")
+#     error = page.locator("h3")
+#     assert error.inner_text() == "Epic sadface: You can only access '/inventory.html' when you are logged in."
+# To trace the browser how it working -> playwright show-trace .\test-results\test-saucedemo-py-test-inventory-chromium\trace.zip
